913d1d3643ce4241fbce83dc1fede1e9035f8218
g g <gabedotgutierrez@gmail.com>
2024-10-19 21:49:21 -0400

    styling added

--- src/components/MailboxList.jsx
+++ src/components/MailboxList.jsx
@@ -5,9 +5,13 @@ function MailboxList(props) {
   return (
     <>
     <div>MailboxList</div>
+    <ul>
       {props.mailboxes.map((mail)=>(
-        <Link key={mail._id} to={`/mailboxes/${mail._id}`}> {mail.boxHolder}</Link>
+        <li className='mail-box'>
+        <Link key={mail._id} to={`/mailboxes/${mail._id}`}>{mail.boxHolder}</Link>
+        </li>
       ))}
+    </ul>  
     </>
   )
 }
